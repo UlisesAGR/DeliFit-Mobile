79878b1e6f9eaa766628a99b0f28e8482770ebf9
ulisesAGR <morromarrito@gmail.com>
2024-03-07 11:58:31 -0600

    [Refactor][Ulises] Remove unused code

--- utils/src/main/java/com/delifit/delifitmobile/utils/SoftKeyboard.kt
+++ utils/src/main/java/com/delifit/delifitmobile/utils/SoftKeyboard.kt
@@ -5,7 +5,6 @@
  */
 package com.delifit.delifitmobile.utils
 
-import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
@@ -15,13 +14,5 @@ fun View.hideSoftKeyboard() {
         val imm =
             context.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(windowToken, 0)
-    }
-}
-
-fun View.showSoftKeyboard(context: Context) {
-    if (context.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) is InputMethodManager) {
-        val imm =
-            context.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
     }
 }
